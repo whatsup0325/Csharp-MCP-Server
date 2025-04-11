da3126c22c3d7e9cfff425cc29a05fa9d1f78b20
zihao <wzihao@unimaxtech.com.tw>
2025-04-11 16:20:00 +0800

    first commit

---
+++ Demo/Demo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using TestServerWithHosting.Tools;
+
+
+
+try
+{
+    Log.Information("Starting server...");
+
+    var builder = Host.CreateApplicationBuilder(args);
+    builder.Services.AddSerilog();
+    builder.Services.AddMcpServer()
+        .WithStdioServerTransport()
+        .WithTools<WorkRecordTool>();
+
+    var app = builder.Build();
+
+    await app.RunAsync();
+    return 0;
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+    return 1;
+}
+finally
+{
+    await Log.CloseAndFlushAsync();
+}
---
+++ Demo/Demo/Tools/WorkRecordTool.cs
@@ -0,0 +1,69 @@
+﻿using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+namespace TestServerWithHosting.Tools;
+
+public class WorkRecord
+{
+    public string Id { get; set; }
+    public DateTime Date { get; set; }
+    public string EmployeeName { get; set; }
+    public string Description { get; set; }
+    public int Hour { get; set; }
+}
+
+
+public static class Data
+{
+    public static List<WorkRecord> workRecords = new List<WorkRecord>
+    {
+        new WorkRecord
+        {
+            Id = "1",
+            Date = new DateTime(2025,4,8),
+            EmployeeName = "Jason",
+            Description = "程式撰寫",
+            Hour = 6
+
+        },
+        new WorkRecord
+        {
+            Id = "2",
+            Date = new DateTime(2025,4,8),
+            EmployeeName = "Jason",
+            Description = "內部會議",
+            Hour = 2
+
+        }
+    };
+}
+
+[McpServerToolType]
+public sealed class WorkRecordTool
+{
+    [McpServerTool(Name = "ListWorkRecord"), Description("Return all work record")]
+    public static IEnumerable<WorkRecord> ListWorkRecord()
+    {
+        return Data.workRecords;
+        }
+
+    [McpServerTool(Name = "AddWorkRecord"), Description("Add work record")]
+    public static void AddWorkRecord(
+        [Description("Working Date")] DateTime date,
+        [Description("Employee code")] string employee,
+        [Description("Job Description")] string description,
+        [Description("Total hours spent")] int hour)
+    {
+        var workRecord = new WorkRecord
+        {
+            Id = Guid.NewGuid().ToString(),
+            Date = date,
+            EmployeeName = employee,
+            Description = description,
+            Hour = hour
+        };
+        Data.workRecords.Add(workRecord);
+    }
+
+
+}
